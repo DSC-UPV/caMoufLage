2711f203d0fb9eb4b52fb7cd3dfc1ab99bfc8874
ENRIQUE TELLO BARBÉ <entelbar@upv.edu.es>
2020-11-25 19:07:23 +0100

    commit2

--- train/gan_model.py
+++ train/gan_model.py
@@ -5,6 +5,7 @@ it will be changed in the near future!
 Source: https://towardsdatascience.com/how-to-build-a-dcgan-with-pytorch-31bfbf2ad96a
 """
 //mi primer cambio
+//segundo cambio
 import os
 import torch
 from torch import nn
